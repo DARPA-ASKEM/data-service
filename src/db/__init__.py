440d6efc2c980ec7234c613a4acb452508769931
Five Grant <5@fivegrant.com>
2022-10-14 14:53:35 -0500

    Init tables from endpoint

--- src/db/__init__.py
+++ src/db/__init__.py
@@ -1,7 +1,7 @@
 from os import getenv
 from sqlalchemy import create_engine
 
-HOST=getenv('SQL_URL', 'localhost')
+HOST=getenv('SQL_URL', 'db')
 PORT=getenv('SQL_PORT','5432')
 USER=getenv('SQL_USER', 'dev')
 PASSWORD=getenv('SQL_PASSWORD', 'dev')
