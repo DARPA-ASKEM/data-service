4fda6e33b6737ed08fee14a50106cbdcff87ad6a
Five Grant <5@fivegrant.com>
2022-11-10 09:33:41 -0600

    Stub out simulation results

--- tds/routers/simulations.py
+++ tds/routers/simulations.py
@@ -5,20 +5,20 @@ tds.router.simulations - crud operations for plans and runs
 from logging import Logger
 from typing import List
 
-from fastapi import APIRouter, Depends, HTTPException, status
+from fastapi import APIRouter, Depends, HTTPException, Response, status
 from sqlalchemy.engine.base import Engine
 from sqlalchemy.orm import Query, Session
 
 from tds.autogen import orm
 from tds.db import entry_exists, request_rdb
-from tds.operation import create, retrieve
-from tds.schema.simulation import Plan
+from tds.operation import create, delete, retrieve
+from tds.schema.simulation import Plan, Results
 
 logger = Logger(__name__)
 router = APIRouter()
 
 
-@router.get("")
+@router.get("/plans")
 def list_plans(rdb: Engine = Depends(request_rdb)) -> List[Plan]:
     """
     Retrieve all plans
@@ -36,7 +36,7 @@ def list_plans(rdb: Engine = Depends(request_rdb)) -> List[Plan]:
     return results
 
 
-@router.get("/{id}", **retrieve.fastapi_endpoint_config)
+@router.get("/plans/{id}", **retrieve.fastapi_endpoint_config)
 def get_plan(id: int, rdb: Engine = Depends(request_rdb)) -> Plan:
     """
     Retrieve plan
@@ -52,7 +52,7 @@ def get_plan(id: int, rdb: Engine = Depends(request_rdb)) -> Plan:
     return Plan.from_orm(plan, list(parameters))
 
 
-@router.post("", **create.fastapi_endpoint_config)
+@router.post("/plans", **create.fastapi_endpoint_config)
 def create_plan(payload: Plan, rdb: Engine = Depends(request_rdb)) -> int:
     """
     Create plan and return its ID
@@ -74,3 +74,52 @@ def create_plan(payload: Plan, rdb: Engine = Depends(request_rdb)) -> int:
         session.commit()
     logger.info("new plan created: %i", id)
     return id
+
+
+@router.get("/results/{id}", **retrieve.fastapi_endpoint_config)
+def get_software(id: int, rdb: Engine = Depends(request_rdb)) -> Results:
+    """
+    Retrieve software metadata
+    """
+    with Session(rdb) as session:
+        if (
+            session.query(orm.SimulationRun).filter(orm.SimulationRun.id == id).count()
+            == 1
+        ):
+            return Results.from_orm(session.query(orm.SimulationRun).get(id))
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND)
+
+
+@router.post("/results", **create.fastapi_endpoint_config)
+def create_software(payload: Results, rdb: Engine = Depends(request_rdb)) -> int:
+    """
+    Create software metadata
+    """
+    with Session(rdb) as session:
+        results_payload = payload.dict()
+        results = orm.SimulationRun(**results_payload)
+        session.add(results)
+        session.commit()
+        id: int = results.id
+    logger.info("new software with %i", id)
+    return id
+
+
+@router.delete("/results/{id}", **delete.fastapi_endpoint_config)
+def delete_software(id: int, rdb: Engine = Depends(request_rdb)) -> Response:
+    """
+    Delete software metadata
+    """
+    with Session(rdb) as session:
+        if (
+            session.query(orm.SimulationRun).filter(orm.SimulationRun.id == id).count()
+            == 1
+        ):
+            results = session.query(orm.SimulationRun).get(id)
+            session.delete(results)
+            session.commit()
+        else:
+            raise HTTPException(status_code=status.HTTP_404_NOT_FOUND)
+    return Response(
+        status_code=status.HTTP_204_NO_CONTENT,
+    )
