b21b90bd386ab5e9e0c6e0c56e0a17ebcab2c1de
Five Grant <5@fivegrant.com>
2022-11-16 08:33:41 -0600

    Reorder models router

--- tds/routers/models.py
+++ tds/routers/models.py
@@ -25,95 +25,6 @@ logger = Logger(__name__)
 router = APIRouter()
 
 
-@router.get("")
-def list_models(rdb: Engine = Depends(request_rdb)) -> List[Model]:
-    """
-    Retrieve all models
-
-    This will return the full list of models, even the previous ones from
-    edit history.
-    """
-    results = []
-    with Session(rdb) as session:
-        for entry in session.query(orm.Model).all():
-            parameters: Query[orm.ModelParameter] = session.query(
-                orm.ModelParameter
-            ).filter(orm.ModelParameter.model_id == entry.id)
-            results.append(Model.from_orm(entry, list(parameters)))
-    return results
-
-
-@router.get("/{id}", **retrieve.fastapi_endpoint_config)
-def get_model(id: int, rdb: Engine = Depends(request_rdb)) -> Model:
-    """
-    Retrieve model
-    """
-    if entry_exists(rdb.connect(), orm.Model, id):
-        with Session(rdb) as session:
-            model = session.query(orm.Model).get(id)
-            parameters: Query[orm.ModelParameter] = session.query(
-                orm.ModelParameter
-            ).filter(orm.ModelParameter.model_id == id)
-    else:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND)
-    return Model.from_orm(model, list(parameters))
-
-
-@router.post("", **create.fastapi_endpoint_config)
-def create_model(payload: Model, rdb: Engine = Depends(request_rdb)) -> Response:
-    """
-    Create model and return its ID
-    """
-    with Session(rdb) as session:
-        model_payload = payload.dict()
-        model_payload.pop("concept")  # TODO: Save ontology term
-        parameters = model_payload.pop("parameters")
-        model_payload.pop("id")
-        model = orm.Model(**model_payload)
-        session.add(model)
-        session.commit()
-        id: int = model.id
-        for name, type in parameters.items():
-            session.add(orm.ModelParameter(model_id=id, name=name, type=type))
-        session.commit()
-    logger.info("new model created: %i", id)
-    return Response(
-        status_code=status.HTTP_201_CREATED,
-        headers={
-            "location": f"/api/model/{id}",
-            "content-type": "application/json",
-        },
-        content=json.dumps({"id": id}),
-    )
-
-
-@router.post("/{id}", **update.fastapi_endpoint_config)
-def update_model(
-    payload: Model,
-    id: int,
-    rdb: Engine = Depends(request_rdb),
-    provenance_handler: ProvenanceHandler = Depends(request_provenance_handler),
-) -> Response:
-    """
-    Update model content
-    """
-    if entry_exists(rdb.connect(), orm.Model, id):
-        new_id = json.loads(create_model(payload, rdb).body)["id"]
-        old_model = get_model(id, rdb)
-        new_model = get_model(new_id, rdb)
-        provenance_handler.create(new_model, old_model, RelationType.editedFrom)
-    else:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND)
-    return Response(
-        status_code=status.HTTP_201_CREATED,
-        headers={
-            "location": f"/api/model/{id}",
-            "content-type": "application/json",
-        },
-        content=json.dumps({"id": new_id}),
-    )
-
-
 @router.get("/frameworks/{name}", **retrieve.fastapi_endpoint_config)
 def get_framework(name: str, rdb: Engine = Depends(request_rdb)) -> ModelFramework:
     """
@@ -223,4 +134,93 @@ def create_intermediate(
             "content-type": "application/json",
         },
         content=json.dumps({"id": id}),
+    )
+
+
+@router.get("")
+def list_models(rdb: Engine = Depends(request_rdb)) -> List[Model]:
+    """
+    Retrieve all models
+
+    This will return the full list of models, even the previous ones from
+    edit history.
+    """
+    results = []
+    with Session(rdb) as session:
+        for entry in session.query(orm.Model).all():
+            parameters: Query[orm.ModelParameter] = session.query(
+                orm.ModelParameter
+            ).filter(orm.ModelParameter.model_id == entry.id)
+            results.append(Model.from_orm(entry, list(parameters)))
+    return results
+
+
+@router.get("/{id}", **retrieve.fastapi_endpoint_config)
+def get_model(id: int, rdb: Engine = Depends(request_rdb)) -> Model:
+    """
+    Retrieve model
+    """
+    if entry_exists(rdb.connect(), orm.Model, id):
+        with Session(rdb) as session:
+            model = session.query(orm.Model).get(id)
+            parameters: Query[orm.ModelParameter] = session.query(
+                orm.ModelParameter
+            ).filter(orm.ModelParameter.model_id == id)
+    else:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND)
+    return Model.from_orm(model, list(parameters))
+
+
+@router.post("", **create.fastapi_endpoint_config)
+def create_model(payload: Model, rdb: Engine = Depends(request_rdb)) -> Response:
+    """
+    Create model and return its ID
+    """
+    with Session(rdb) as session:
+        model_payload = payload.dict()
+        model_payload.pop("concept")  # TODO: Save ontology term
+        parameters = model_payload.pop("parameters")
+        model_payload.pop("id")
+        model = orm.Model(**model_payload)
+        session.add(model)
+        session.commit()
+        id: int = model.id
+        for name, type in parameters.items():
+            session.add(orm.ModelParameter(model_id=id, name=name, type=type))
+        session.commit()
+    logger.info("new model created: %i", id)
+    return Response(
+        status_code=status.HTTP_201_CREATED,
+        headers={
+            "location": f"/api/model/{id}",
+            "content-type": "application/json",
+        },
+        content=json.dumps({"id": id}),
+    )
+
+
+@router.post("/{id}", **update.fastapi_endpoint_config)
+def update_model(
+    payload: Model,
+    id: int,
+    rdb: Engine = Depends(request_rdb),
+    provenance_handler: ProvenanceHandler = Depends(request_provenance_handler),
+) -> Response:
+    """
+    Update model content
+    """
+    if entry_exists(rdb.connect(), orm.Model, id):
+        new_id = json.loads(create_model(payload, rdb).body)["id"]
+        old_model = get_model(id, rdb)
+        new_model = get_model(new_id, rdb)
+        provenance_handler.create(new_model, old_model, RelationType.editedFrom)
+    else:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND)
+    return Response(
+        status_code=status.HTTP_201_CREATED,
+        headers={
+            "location": f"/api/model/{id}",
+            "content-type": "application/json",
+        },
+        content=json.dumps({"id": new_id}),
     )
--- tests/routers/models.py
+++ tests/routers/models.py
@@ -2,7 +2,8 @@
 Tests basic model crud
 """
 
-from tds.autogen.schema import ValueType
+from tds.autogen.schema import IntermediateFormat, IntermediateSource, ValueType
+from tds.schema.model import Intermediate, ModelFramework
 from tests.helpers import demo_api
 
 
@@ -60,3 +61,73 @@ def test_model_cru():
             "y" in new_payload["parameters"]
             and new_payload["parameters"]["y"] == ValueType.bool
         )
+
+
+def test_framework_crd():
+    """
+    Test creation, retrieval and delete operations for framework.
+
+    Note: There currently isn't a way to modify intermediates so an
+          update has not been implemented
+    """
+    with demo_api("models") as client:
+        # Creation
+        payload = {"name": "dummy", "version": "v0", "semantics": ""}
+        response_post = client.post(
+            "/models/frameworks",
+            json=payload,
+            headers={"Content-type": "application/json", "Accept": "text/plain"},
+        )
+        assert 200 == response_post.status_code
+        id = response_post.json()
+        # Retrieval
+        response_get = client.get(
+            f"/models/frameworks/{id}", headers={"Accept": "application/json"}
+        )
+        assert 200 == response_get.status_code
+        result = ModelFramework.parse_obj(response_get.json())
+        assert payload["name"] == result.name
+        # Deletion
+        response_delete = client.delete(f"/models/frameworks/{id}")
+        assert 204 == response_delete.status_code
+        response_get_fail = client.get(
+            f"/models/frameworks/{id}", headers={"Accept": "application/json"}
+        )
+        assert response_get_fail.status_code == 404
+
+
+def test_intermediate_crd():
+    """
+    Test creation, retrieval and delete operations for intermediates.
+
+    Note: There currently isn't a way to modify intermediates so an
+          update has not been implemented
+    """
+    with demo_api("models") as client:
+        # Creation
+        payload = {
+            "source": IntermediateSource.skema,
+            "type": IntermediateFormat.gromet,
+            "content": "",
+        }
+        response_post = client.post(
+            "/models/intermediates",
+            json=payload,
+            headers={"Content-type": "application/json", "Accept": "text/plain"},
+        )
+        assert 201 == response_post.status_code
+        id = response_post.json()
+        # Retrieval
+        response_get = client.get(
+            f"/models/intermediates/{id}", headers={"Accept": "application/json"}
+        )
+        assert 200 == response_get.status_code
+        result = Intermediate.parse_obj(response_get.json())
+        assert Intermediate.source == result.source and Intermediate.type == result.type
+        # Deletion
+        response_delete = client.delete(f"/models/intermediates/{id}")
+        assert 204 == response_delete.status_code
+        response_get_fail = client.get(
+            f"/models/intermediates/{id}", headers={"Accept": "application/json"}
+        )
+        assert response_get_fail.status_code == 404
