1d4f1e55a649503202df079db486fdd0cc4b8c63
Five Grant <5@fivegrant.com>
2022-11-18 13:58:34 -0600

    Add model description

--- tds/routers/models.py
+++ tds/routers/models.py
@@ -15,11 +15,12 @@ from tds.autogen.schema import RelationType
 from tds.db import (
     ProvenanceHandler,
     entry_exists,
+    list_by_id,
     request_provenance_handler,
     request_rdb,
 )
 from tds.operation import create, delete, retrieve, update
-from tds.schema.model import Intermediate, Model, ModelFramework
+from tds.schema.model import Intermediate, Model, ModelDescription, ModelFramework
 
 logger = Logger(__name__)
 router = APIRouter()
@@ -137,22 +138,30 @@ def delete_intermediate(id: int, rdb: Engine = Depends(request_rdb)) -> Response
     )
 
 
-@router.get("")
-def list_models(rdb: Engine = Depends(request_rdb)) -> List[Model]:
+@router.get("/descriptions")
+def list_model_descriptions(
+    page: int = 100, page_size: int = 0, rdb: Engine = Depends(request_rdb)
+) -> List[Model]:
     """
     Retrieve all models
 
     This will return the full list of models, even the previous ones from
     edit history.
     """
-    results = []
-    with Session(rdb) as session:
-        for entry in session.query(orm.Model).all():
-            parameters: Query[orm.ModelParameter] = session.query(
-                orm.ModelParameter
-            ).filter(orm.ModelParameter.model_id == entry.id)
-            results.append(Model.from_orm(entry, list(parameters)))
-    return results
+    return list_by_id(rdb.connect(), orm.Model, page, page_size)
+
+
+@router.get("/descriptions/{id}", **retrieve.fastapi_endpoint_config)
+def get_model_description(id: int, rdb: Engine = Depends(request_rdb)) -> Model:
+    """
+    Retrieve model
+    """
+    if entry_exists(rdb.connect(), orm.Model, id):
+        with Session(rdb) as session:
+            model = session.query(orm.Model).get(id)
+    else:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND)
+    return ModelDescription.from_orm(model)
 
 
 @router.get("/{id}", **retrieve.fastapi_endpoint_config)
