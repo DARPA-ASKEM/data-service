114af8ef92076eda622b03187708529f660e0b93
Five Grant <5@fivegrant.com>
2022-11-29 09:40:57 -0600

    Bugfix facet search

--- tds/routers/concepts.py
+++ tds/routers/concepts.py
@@ -8,7 +8,7 @@ from typing import List, Optional
 from urllib.parse import quote_plus
 
 import requests
-from fastapi import APIRouter, Depends, Response, status
+from fastapi import APIRouter, Depends, Query, Response, status
 from sqlalchemy import func
 from sqlalchemy.engine.base import Engine
 from sqlalchemy.orm import Session
@@ -42,39 +42,49 @@ def search_concept(curie: str, rdb: Engine = Depends(request_rdb)):
 
 @router.get("/facets")
 def search_concept_using_facets(
-    types: Optional[List[schema.TaggableType]] = None,
-    curies: Optional[List[str]] = None,
+    types: List[schema.TaggableType] = Query(default=None),
+    curies: Optional[List[str]] = Query(default=None),
     rdb: Engine = Depends(request_rdb),
-):
+) -> Response:
     """
     Search along type and curie facets
     """
     with Session(rdb) as session:
         search_body = {
             "types": session.query(
-                func.count(orm.OntologyConcept.type), orm.OntologyConcept
+                func.count(orm.OntologyConcept.type), orm.OntologyConcept.type
             ).group_by(orm.OntologyConcept.type),
             "curies": session.query(
-                func.count(orm.OntologyConcept.curie), orm.OntologyConcept
+                func.count(orm.OntologyConcept.curie), orm.OntologyConcept.curie
             ).group_by(orm.OntologyConcept.curie),
             "results": session.query(orm.OntologyConcept),
         }
         for key in search_body:
             if types is not None:
                 search_body[key] = search_body[key].filter(
-                    orm.OntologyConcept.type in types
+                    orm.OntologyConcept.type.in_(types)
                 )
             if curies is not None:
                 search_body[key] = search_body[key].filter(
-                    orm.OntologyConcept.curie in curies
+                    orm.OntologyConcept.curie.in_(curies)
                 )
-            search_body[key] = search_body[key].all()
         return Response(
             status_code=status.HTTP_200_OK,
             headers={
                 "content-type": "application/json",
             },
-            content=json.dumps(search_body),
+            content=json.dumps(
+                {
+                    "facets": {
+                        "types": [tuple(hit) for hit in search_body["types"]],
+                        "curies": [tuple(hit) for hit in search_body["curies"]],
+                    },
+                    "results": [
+                        (entry.type, entry.object_id, entry.curie)
+                        for entry in search_body["results"]
+                    ],
+                }
+            ),
         )
 
 
