80d12587868f6a9dc6d89296b96b0b814cfdd554
Five Grant <5@fivegrant.com>
2022-11-29 10:55:43 -0600

    Increase pool size (#77)

--- tds/db/relational.py
+++ tds/db/relational.py
@@ -8,7 +8,9 @@ from tds.settings import settings
 
 # pylint: disable-next=line-too-long
 url = f"postgresql://{settings.SQL_USER}:{settings.SQL_PASSWORD}@{settings.SQL_URL}:{settings.SQL_PORT}/askem"
-engine = create_engine(url, connect_args={"connect_timeout": 8})
+engine = create_engine(
+    url, pool_size=25, max_overflow=10, connect_args={"connect_timeout": 8}
+)
 
 
 async def request_engine():
