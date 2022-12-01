a3a08a223fb2144f778434b372e7f370f7870dfd
Five Grant <5@fivegrant.com>
2022-12-01 14:53:54 -0600

    Add basic model and project functionality

--- tds/routers/experimental.py
+++ tds/routers/experimental.py
@@ -3,29 +3,80 @@ Experimental GraphQL router
 """
 
 from logging import Logger
-from typing import List
+from typing import Any, Dict, List
 
 import strawberry
 from fastapi import APIRouter, Depends, HTTPException, Response, status
 from sqlalchemy.engine.base import Engine
-from sqlalchemy.orm import Query, Session
+from sqlalchemy.orm import Session
 from strawberry.fastapi import GraphQLRouter
+from strawberry.types import Info
 
-from tds.autogen import orm
-from tds.db import entry_exists, list_by_id, request_rdb
-from tds.lib.simulations import adjust_run_params
-from tds.operation import create, retrieve, update
+from tds.autogen import orm, schema
+from tds.db import list_by_id, request_rdb
+from tds.schema.model import ModelDescription
+from tds.schema.project import ProjectMetadata
 
 logger = Logger(__name__)
 
 
+@strawberry.experimental.pydantic.type(model=ModelDescription)
+class Model:
+    id: strawberry.auto
+    name: strawberry.auto
+    description: strawberry.auto
+    framework: strawberry.auto
+    timestamp: strawberry.auto
+    content: str
+
+    @staticmethod
+    def from_pydantic(instance: ModelDescription) -> "Model":
+        data = instance.dict()
+        data["content"] = str(data["content"])
+        data.pop("concept")  # TODO: Include
+        return Model(**data)
+
+
+def list_models(info: Info) -> List[Model]:
+    fetched_models: List[orm.Model] = list_by_id(
+        info.context["rdb"].connect(), orm.Model, 100, 0
+    )
+    to_graphql = lambda model: Model.from_pydantic(ModelDescription.from_orm(model))
+    return [to_graphql(model) for model in fetched_models]
+
+
+@strawberry.experimental.pydantic.type(model=ProjectMetadata)
+class Project:
+    id: strawberry.auto
+    name: strawberry.auto
+    description: strawberry.auto
+    timestamp: strawberry.auto
+    active: strawberry.auto
+
+
+def list_projects(info: Info) -> List[Project]:
+    fetched_projects: List[orm.Project] = list_by_id(
+        info.context["rdb"].connect(), orm.Project, 100, 0
+    )
+    to_graphql = lambda model: Project.from_pydantic(ProjectMetadata.from_orm(model))
+    return [to_graphql(proj) for proj in fetched_projects]
+
+
 @strawberry.type
 class Query:
-    @strawberry.field
-    def hello(self) -> str:
-        return "Hello World"
+    projects: List[Project] = strawberry.field(resolver=list_projects)
+    models: List[Model] = strawberry.field(resolver=list_models)
 
 
 schema = strawberry.Schema(query=Query)
 
-router = GraphQLRouter(schema)
+
+async def get_context(
+    rdb=Depends(request_rdb),
+):
+    return {
+        "rdb": rdb,
+    }
+
+
+router = GraphQLRouter(schema, context_getter=get_context)
