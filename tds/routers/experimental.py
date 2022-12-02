1aac0ac7eb8bcd469a7b33c52b9a558ae6d24e24
Five Grant <5@fivegrant.com>
2022-12-02 08:34:39 -0600

    (BREAKS) Add parameters to strawberry model

--- tds/routers/experimental.py
+++ tds/routers/experimental.py
@@ -14,13 +14,22 @@ from strawberry.types import Info
 
 from tds.autogen import orm, schema
 from tds.db import list_by_id, request_rdb
-from tds.schema.model import ModelDescription
+from tds.schema import model
 from tds.schema.project import ProjectMetadata
 
 logger = Logger(__name__)
 
 
-@strawberry.experimental.pydantic.type(model=ModelDescription)
+@strawberry.type
+class Parameter:
+    id: int
+    name: str
+    type: schema.ValueType
+    default_value: str
+    state_variable: bool
+
+
+@strawberry.experimental.pydantic.type(model=model.Model)
 class Model:
     id: strawberry.auto
     name: strawberry.auto
@@ -28,20 +37,21 @@ class Model:
     framework: strawberry.auto
     timestamp: strawberry.auto
     content: str
+    parameters: List[Parameter]
 
     @staticmethod
-    def from_pydantic(instance: ModelDescription) -> "Model":
+    def from_pydantic(instance: model.Model) -> "Model":
         data = instance.dict()
         data["content"] = str(data["content"])
         data.pop("concept")  # TODO: Include
         return Model(**data)
 
 
-def list_models(info: Info) -> List[Model]:
+def list_models(info: Info) -> List["Model"]:
     fetched_models: List[orm.Model] = list_by_id(
         info.context["rdb"].connect(), orm.Model, 100, 0
     )
-    to_graphql = lambda model: Model.from_pydantic(ModelDescription.from_orm(model))
+    to_graphql = lambda model: Model.from_pydantic(model.Model.from_orm(model))
     return [to_graphql(model) for model in fetched_models]
 
 
@@ -60,6 +70,10 @@ def list_projects(info: Info) -> List[Project]:
     )
     to_graphql = lambda model: Project.from_pydantic(ProjectMetadata.from_orm(model))
     return [to_graphql(proj) for proj in fetched_projects]
+
+
+def list_assets(root: Project, info: Info) -> None:
+    pass
 
 
 @strawberry.type
