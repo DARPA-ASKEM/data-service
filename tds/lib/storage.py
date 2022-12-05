8a7dc3fec108981b6851da2d6768d66b80933a0f
Powell <powellfendley@gmail.com>
2022-12-05 16:39:15 -0500

    Updated conversion code for wide-format by using slightly different pandas functions, regenerated .sql file

--- tds/lib/storage.py
+++ tds/lib/storage.py
@@ -127,14 +127,17 @@ def stream_csv_from_data_paths(dataframe, wide_format=False):
         copy=False,
     )
     if wide_format:
-        dataframe_wide = pandas.pivot(
-            dataframe, index=None, columns="feature", values="value"
-        )  # Reshape from long to wide
-        print(dataframe_wide)
-        dataframe = dataframe.drop(["feature", "value"], axis=1)
-        dataframe = pandas.merge(
-            dataframe, dataframe_wide, left_index=True, right_index=True
-        )
+        dataframe_wide = pandas.pivot_table(
+            dataframe,
+            index=["timestamp", "country", "admin1", "admin2", "admin3", "lat", "lng"],
+            columns="feature",
+            values="value",
+            aggfunc="first",
+        ).reset_index()  # Reshape from long to wide
 
-    output = dataframe.to_csv(index=False)
+        print(dataframe_wide)
+
+        output = dataframe_wide.to_csv(index=False)
+    else:
+        output = dataframe.to_csv(index=False)
     return output
