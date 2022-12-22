6c7d76001c226ba16d0eb6d37e05307695ddd3a1
Five Grant <5@fivegrant.com>
2022-12-22 12:34:45 -0600

    Refactor tests (#134)

--- tests/helpers.py
+++ tests/helpers.py
@@ -4,7 +4,7 @@ The reusable Test DB connection
 import os
 from contextlib import contextmanager
 from sqlite3 import connect
-from typing import Generator
+from typing import Generator, Tuple
 
 from fastapi.testclient import TestClient
 from sqlalchemy import create_engine
@@ -20,10 +20,12 @@ def demo_rdb() -> Generator[Engine, None, None]:
     Wraps for generating an in-memory DB for running SQL-related tests
     """
 
+    if os.path.exists(".sqlite"):
+        os.remove(".sqlite")
+
     engine = create_engine(
         "sqlite:///.sqlite",
         creator=lambda: connect(
-            # "file:test:?mode=memory&cache=shared", uri=True, check_same_thread=False
             "file:.sqlite",
             uri=True,
             check_same_thread=False,
@@ -55,3 +57,37 @@ def demo_api(*router_name: str) -> Generator[TestClient, None, None]:
         api.dependency_overrides[request_rdb] = request_test_rdb
         api.dependency_overrides[request_graph_db] = request_test_graph
         yield TestClient(api)
+
+
+@contextmanager
+def demo_api_context(
+    *router_name: str,
+) -> Generator[Tuple[TestClient, Engine], None, None]:
+    """
+    Environment for testing a router
+    """
+    api = build_api(*router_name)
+    rdb = create_engine(
+        "sqlite:///.sqlite",
+        creator=lambda: connect(
+            "file:.sqlite",
+            uri=True,
+            check_same_thread=False,
+        ),
+    )
+    connection = rdb.connect()
+    init_dev_content(connection)
+
+    async def request_test_rdb():
+        yield rdb
+
+    async def request_test_graph():
+        yield None
+
+    api.dependency_overrides[request_rdb] = request_test_rdb
+    api.dependency_overrides[request_graph_db] = request_test_graph
+    try:
+        yield (TestClient(api), rdb)
+    finally:
+        connection.close()
+        os.remove(".sqlite")
--- tests/unittest.py
+++ tests/unittest.py
@@ -1,6 +1,10 @@
 """
-Test provenance wrapper
+Validate unit tests
 """
+
+from dbml_builder import get_dbml_version
+from fastapi.testclient import TestClient
+
 from tds.autogen.schema import (
     IntermediateFormat,
     IntermediateSource,
@@ -9,10 +13,31 @@ from tds.autogen.schema import (
 )
 from tds.db import ProvenanceHandler
 from tds.schema.resource import Intermediate, Publication, get_resource_type
+from tds.server.build import build_api
+from tds.settings import settings
 from tests.helpers import demo_rdb
 
 
+def test_version() -> None:
+    """
+    Ensure the code is not using an outdated version of the DBML
+    """
+    assert settings.DBML_VERSION == get_dbml_version(settings.DBML_PATH)
+
+
+def test_build_api() -> None:
+    """
+    Ensure that the API can be built
+    """
+    client = TestClient(build_api())
+    response = client.get("/")
+    assert response.status_code == 200
+
+
 def test_relation_handler_rdb_only():
+    """
+    Ensure the provenance handler can create a basic edge
+    """
     with demo_rdb() as rdb:
         relation_handler = ProvenanceHandler(rdb, None)
         intermediate = Intermediate(
