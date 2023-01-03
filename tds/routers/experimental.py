54ee860f71df2dfbbd1e504da873f7b654a3e4a6
Five Grant <5@fivegrant.com>
2023-01-03 11:00:02 -0600

    Update enum names

--- tds/routers/experimental.py
+++ tds/routers/experimental.py
@@ -22,43 +22,43 @@ router = APIRouter()
 
 valid_relations = {
     orm.RelationType.COPIED_FROM: [
-        (orm.ProvenanceType.Model_revision, orm.ProvenanceType.Model_revision)
+        (orm.ProvenanceType.ModelRevision, orm.ProvenanceType.ModelRevision)
     ],
     orm.RelationType.CITES: [
         (orm.ProvenanceType.Publication, orm.ProvenanceType.Publication)
     ],
     orm.RelationType.COMBINED_FROM: [
-        (orm.ProvenanceType.Model_revision, orm.ProvenanceType.Model_revision)
+        (orm.ProvenanceType.ModelRevision, orm.ProvenanceType.ModelRevision)
     ],
     orm.RelationType.EDITED_FROM: [
-        (orm.ProvenanceType.Model_revision, orm.ProvenanceType.Model_revision)
+        (orm.ProvenanceType.ModelRevision, orm.ProvenanceType.ModelRevision)
     ],
     orm.RelationType.USES: [
-        (orm.ProvenanceType.Plan, orm.ProvenanceType.Model_revision)
+        (orm.ProvenanceType.Plan, orm.ProvenanceType.ModelRevision)
     ],
     orm.RelationType.EXTRACTED_FROM: [
         (orm.ProvenanceType.Intermediate, orm.ProvenanceType.Publication),
         (orm.ProvenanceType.Dataset, orm.ProvenanceType.Publication),
     ],
     orm.RelationType.REINTERPRETS: [
-        (orm.ProvenanceType.Dataset, orm.ProvenanceType.Simulation_run),
+        (orm.ProvenanceType.Dataset, orm.ProvenanceType.SimulationRun),
         (orm.ProvenanceType.Intermediate, orm.ProvenanceType.Intermediate),
         (orm.ProvenanceType.Model, orm.ProvenanceType.Intermediate),
     ],
     orm.RelationType.GENERATED_BY: [
-        (orm.ProvenanceType.Simulation_run, orm.ProvenanceType.Plan)
+        (orm.ProvenanceType.SimulationRun, orm.ProvenanceType.Plan)
     ],
     orm.RelationType.BEGINS_AT: [
-        (orm.ProvenanceType.Model, orm.ProvenanceType.Model_revision)
+        (orm.ProvenanceType.Model, orm.ProvenanceType.ModelRevision)
     ],
     orm.RelationType.DECOMPOSED_FROM: [
-        (orm.ProvenanceType.Model_revision, orm.ProvenanceType.Model_revision)
+        (orm.ProvenanceType.ModelRevision, orm.ProvenanceType.ModelRevision)
     ],
     orm.RelationType.GLUED_FROM: [
-        (orm.ProvenanceType.Model_revision, orm.ProvenanceType.Model_revision)
+        (orm.ProvenanceType.ModelRevision, orm.ProvenanceType.ModelRevision)
     ],
     orm.RelationType.STRATIFIED_FROM: [
-        (orm.ProvenanceType.Model_revision, orm.ProvenanceType.Model_revision)
+        (orm.ProvenanceType.ModelRevision, orm.ProvenanceType.ModelRevision)
     ],
 }
 
@@ -87,10 +87,10 @@ Question: Return simulators that rely on Primitive with ID of 19 or 12
 Query: `match (p:Plan)-[r *1..]->(i:Intermediate) where i.id=19 or i.id=12 return p,r,i`
 
 Question: Which simulators were created by User with ID 999?
-Query: `match (p:Plan)-[r:USES]->(mr:Model_revision) where r.user_id=999 return p`
+Query: `match (p:Plan)-[r:USES]->(mr:ModelRevision) where r.user_id=999 return p`
 
 Question: What are prior versions of this composed model with id 5?
-Query: `Match (rev:Model_revision)<-[r:BEGINS_AT]-(m:Model) where (m.id=5) match (rev2:Model_revision)<-[r2 *1.. ]-(rev) return rev,rev2`
+Query: `Match (rev:ModelRevision)<-[r:BEGINS_AT]-(m:Model) where (m.id=5) match (rev2:ModelRevision)<-[r2 *1.. ]-(rev) return rev,rev2`
 """
 
 
