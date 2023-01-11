c8b85ea4f3b1901ac17092df5a64c67684026788
Five Grant <5@fivegrant.com>
2023-01-11 16:35:01 -0600

    Load data on startup

--- docker/scripts/populate-cache.cypher
+++ docker/scripts/populate-cache.cypher
@@ -4,4 +4,4 @@ CALL apoc.merge.node([row.left_type],{id: row.left}) yield node as l
 CALL apoc.merge.node([row.right_type],{id: row.right}) yield node as r
 // TODO: Include user_id
 CALL apoc.merge.relationship(l,row.relation_type, {},{}, r) yield rel as relation
-return relation, l, r
+return relation, l, r;
--- docker/scripts/start-neo4j.sh
+++ docker/scripts/start-neo4j.sh
@@ -1,8 +1,16 @@
 #!/bin/bash
 
 # TODO: Run the script dynamically when Postgres is available instead of sleeping
-sleep 5
+sleep 2
 
 psql --command="\COPY provenance TO '/var/lib/neo4j/import/provenance.csv' WITH (FORMAT CSV, HEADER);" postgresql://dev:dev@rdb:5432/askem
 
-/startup/docker-entrypoint.sh neo4j
+set -m
+/startup/docker-entrypoint.sh neo4j &
+
+# TODO: run this using apoc.initialize
+sleep 8
+cypher-shell -u neo4j -p password -f /tmp/populate-cache.cypher --non-interactive
+echo "Loaded all data"
+
+fg %1
