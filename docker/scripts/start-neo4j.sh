28e35d35ba683c21648fc1f0891f005a29caf501
Matthew Printz <matt@jataware.com>
2023-01-12 09:35:16 -0600

    Wait for service to be available instead of constant sleep

--- docker/scripts/start-neo4j.sh
+++ docker/scripts/start-neo4j.sh
@@ -1,15 +1,21 @@
 #!/bin/bash
 
-# TODO: Run the script dynamically when Postgres is available instead of sleeping
-sleep 2
-
+echo Waiting for rdb to be available
+while ! nc -z rdb 5432; do
+    sleep 0.2
+done
+echo rdb is now available. Continuing.
 psql --command="\COPY provenance TO '/var/lib/neo4j/import/provenance.csv' WITH (FORMAT CSV, HEADER);" postgresql://dev:dev@rdb:5432/askem
 
 set -m
 /startup/docker-entrypoint.sh neo4j &
 
 # TODO: run this using apoc.initialize
-sleep 15
+echo Waiting for neo4j service to be available
+while ! nc -z localhost 7474; do
+    sleep 0.2
+done
+echo neo4j is now available. Continuing.
 cypher-shell -u neo4j -p password -f /tmp/populate-cache.cypher --non-interactive
 echo "Loaded all data"
 
