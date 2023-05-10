3663833efc4c873c9d16159cacfc8d7223056725
Todd Roper <toddroper@me.com>
2023-05-10 10:31:28 -0700

    Refactored model to account for new structure.

--- tds/model/model.py
+++ tds/model/model.py
@@ -2,9 +2,30 @@ from typing import List, Optional
 
 from pydantic import BaseModel
 
+from tds.autogen import orm
+from tds.db.base import TdsModel
+from tds.model import orm_to_params
 
-class Model(BaseModel):
-    id: Optional[int]
+
+class Model(TdsModel):
     name: str
     description: str
-    parameters: Optional[List]
+    model: dict
+
+    _index = "model"
+
+    # def __init__(self, **kwargs):
+    #     for kwarg in kwargs:
+    #         setattr(self, kwarg, kwargs[kwarg])
+
+    @classmethod
+    def from_orm(
+        cls,
+        parameters: List,
+    ) -> "Model":
+        """
+        Handle ORM conversion while coercing `dict` to JSON
+        """
+        body.__dict__["content"] = dumps(ModelContent.from_orm(state).content)
+        body.__dict__["parameters"] = orm_to_params(parameters)
+        return super().from_orm(body)
