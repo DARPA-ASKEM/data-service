fb83d53bea0a661442fadd3bbdfeb6e500b7d1f0
Todd Roper <toddroper@me.com>
2023-05-11 07:23:53 -0700

    Added concept logic to model save.

--- tds/modules/model/model.py
+++ tds/modules/model/model.py
@@ -1,9 +1,12 @@
 from typing import List, Optional
 
-from pydantic import BaseModel, Field
+from pydantic import Field
+from sqlalchemy.orm import Query, Session
 
 from tds.autogen import orm
 from tds.db.base import TdsModel
+from tds.db.relational import engine as pg_engine
+from tds.lib.concepts import mark_concept_active
 from tds.modules.model.utils import orm_to_params
 
 
@@ -30,20 +33,51 @@ class Model(TdsModel):
         return super().from_orm(body)
 
     def save(self, id: Optional[None | str | int] = None):
-        self._extract_concepts()
-        return super(Model, self).save(id)
+        res = super(Model, self).save(id)
+        # Pass the model id so we have it for association.
+        self._extract_concepts(res["_id"])
+        return res
 
-    def _extract_concepts(self):
+    def _extract_concepts(self, model_id):
+        """
+        Method extracts concepts from the model and saves them to the db.
+        """
         states = self.model["states"]
+        curies = []
+        with Session(pg_engine) as pg_db:
+            for state in states:
+                if (
+                    "grounding" in state
+                    and "identifiers" in state["grounding"]
+                    and bool(state["grounding"]["identifiers"])
+                ):
+                    for key in state["grounding"]["identifiers"]:
+                        value = state["grounding"]["identifiers"][key]
+                        curie = f"{key}:{value}"
+                        # @TODO: Break this code out for reuse where other data types can use it to handle concepts.
+                        concept = (
+                            pg_db.query(orm.ActiveConcept)
+                            .filter(orm.ActiveConcept.curie == curie)
+                            .first()
+                        )
+                        if concept is None:
+                            mark_concept_active(pg_db, curie)
+                            concept = (
+                                pg_db.query(orm.ActiveConcept)
+                                .filter(orm.ActiveConcept.curie == curie)
+                                .first()
+                            )
 
-        for state in states:
-            if (
-                "grounding" in state
-                and "identifiers" in state["grounding"]
-                and bool(state["grounding"]["identifiers"])
-            ):
-                for key in state["grounding"]["identifiers"]:
-                    value = state["grounding"]["identifiers"][key]
-                    curie = f"{key}:{value}"
-                    if curie not in self.concepts:
-                        self.concepts.append(curie)
+                        if concept.name not in self.concepts:
+                            curies.append(
+                                curie
+                            )  # Append to local list to prevent repeated queries.
+                            concept_association = orm.OntologyConcept(
+                                curie=curie,
+                                type="models",
+                                object_id=model_id,
+                                status="obj",
+                            )
+                            pg_db.add(concept_association)
+                            pg_db.commit()
+                            self.concepts.append(concept.name)
