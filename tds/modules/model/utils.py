510ed83e1520ef05786b7d6afed6e5cc391a779d
Todd Roper <toddroper@me.com>
2023-05-11 16:56:56 -0700

    Added delete fields to model response to replicate other endpoints.

--- tds/modules/model/utils.py
+++ tds/modules/model/utils.py
@@ -23,14 +23,23 @@ def orm_to_params(parameters: List):
     ]
 
 
-def model_response(model_from_es):
+def model_response(model_from_es, delete_fields=[]):
     """
     Function builds model response object from an ElasticSearch model.
     """
     es_response = model_from_es.body
     model = es_response["_source"]
-    model["id"] = es_response["_id"]
+    model["state_id"] = model["id"] = es_response["_id"]
+    frameworks = get_frameworks()
+    model["framework"] = frameworks[model["model_schema"]]
+    model["schema"] = model["model_schema"]
+
+    del model["model_schema"]
     del model["concepts"]
+
+    if delete_fields:
+        for field in delete_fields:
+            del model[field]
 
     return model
 
@@ -41,11 +50,8 @@ def model_list_response(model_list_from_es):
     """
     df = pd.DataFrame(model_list_from_es)
     fields = pd.json_normalize(df["fields"])
-    with Session(pg_engine) as pg_db:
-        frameworks = pg_db.query(orm.ModelFramework).all()
-        framework_map = {}
-        for f in frameworks:
-            framework_map[f.schema_url] = f.name
+
+    framework_map = get_frameworks()
 
     for col in fields.columns:
         fields[col] = fields[col].map(lambda x: x[0])
@@ -53,3 +59,12 @@ def model_list_response(model_list_from_es):
             fields["framework"] = fields[col].map(lambda x: framework_map[x])
 
     return fields.to_json(orient="records")
+
+
+def get_frameworks():
+    with Session(pg_engine) as pg_db:
+        frameworks = pg_db.query(orm.ModelFramework).all()
+        framework_map = {}
+        for f in frameworks:
+            framework_map[f.schema_url] = f.name
+    return framework_map
