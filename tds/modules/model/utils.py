16ed30fb59582b20168cc76aef5a1eb0a6ce7447
Todd Roper <toddroper@me.com>
2023-05-11 16:35:33 -0700

    Adding model response handler.

--- tds/modules/model/utils.py
+++ tds/modules/model/utils.py
@@ -1,5 +1,11 @@
 from typing import List
 
+import pandas as pd
+from sqlalchemy.orm import Query, Session
+
+from tds.autogen import orm
+from tds.db.relational import engine as pg_engine
+
 
 def orm_to_params(parameters: List):
     """
@@ -27,3 +33,23 @@ def model_response(model_from_es):
     del model["concepts"]
 
     return model
+
+
+def model_list_response(model_list_from_es):
+    """
+    Function builds model response object from an ElasticSearch model.
+    """
+    df = pd.DataFrame(model_list_from_es)
+    fields = pd.json_normalize(df["fields"])
+    with Session(pg_engine) as pg_db:
+        frameworks = pg_db.query(orm.ModelFramework).all()
+        framework_map = {}
+        for f in frameworks:
+            framework_map[f.schema_url] = f.name
+
+    for col in fields.columns:
+        fields[col] = fields[col].map(lambda x: x[0])
+        if col == "model_schema":
+            fields["framework"] = fields[col].map(lambda x: framework_map[x])
+
+    return fields.to_json(orient="records")
