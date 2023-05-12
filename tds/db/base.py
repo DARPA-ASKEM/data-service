6a0860a63f909ef9e64e6667ce9d23688cf70d22
Todd Roper <toddroper@me.com>
2023-05-11 17:14:39 -0700

    Added timestamp field to save.

--- tds/db/base.py
+++ tds/db/base.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 from typing import Optional
 
 from pydantic import BaseModel
@@ -8,8 +9,10 @@ from tds.db import es
 class TdsModel(BaseModel):
     id: Optional[int | str]
     _index: str
+    timestamp: Optional[datetime]
 
     def save(self, id: Optional[None | str | int] = None):
+        self.timestamp = datetime.now()
         if self.id or id:
             res = es.index(
                 index=self._index, body=self.dict(), id=(id if id else self.id)
