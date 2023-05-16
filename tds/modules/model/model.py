a699424438d37cd8bad0df49329c6f651722a2b1
Todd Roper <toddroper@me.com>
2023-05-16 08:20:06 -0700

    Added swagger post example.

--- tds/modules/model/model.py
+++ tds/modules/model/model.py
@@ -93,3 +93,60 @@ class Model(TdsModel):
             )
 
             provenance_handler.create_entry(payload)
+
+    class Config:
+        schema_extra = {
+            "example": {
+                "name": "A Test Model",
+                "schema": "https://raw.githubusercontent.com/DARPA-ASKEM/Model-Representations/petrinet_v0.2/petrinet/petrinet_schema.json",
+                "description": "Test Model Post from Swagger.",
+                "model_version": "1.0",
+                "model": {
+                    "states": [
+                        {
+                            "id": "S",
+                            "name": "S",
+                            "grounding": {
+                                "identifiers": {"ncbitaxon": "9606"},
+                                "context": {},
+                            },
+                        },
+                        {
+                            "id": "V",
+                            "name": "V",
+                            "grounding": {
+                                "identifiers": {"ncbitaxon": "9606"},
+                                "context": {},
+                            },
+                        },
+                    ],
+                    "transitions": [
+                        {
+                            "id": "t1",
+                            "input": ["S"],
+                            "output": [],
+                            "properties": {
+                                "name": "t1",
+                                "rate": {
+                                    "expression": "S/l_e",
+                                    "expression_mathml": "<apply><divide/><ci>S</ci><ci>l_e</ci></apply>",
+                                },
+                            },
+                        },
+                        {
+                            "id": "t2",
+                            "input": ["V"],
+                            "output": [],
+                            "properties": {
+                                "name": "t2",
+                                "rate": {
+                                    "expression": "V/l_e",
+                                    "expression_mathml": "<apply><divide/><ci>V</ci><ci>l_e</ci></apply>",
+                                },
+                            },
+                        },
+                    ],
+                    "parameters": [{"id": "l_e", "value": 50.0}],
+                },
+            }
+        }
