4524f12db13431ce6e7c9343a0eb13eaab3b0dfe
Matthew Printz <matt@jataware.com>
2023-05-16 14:03:01 -0500

    Wip

--- dataset-schema.py
+++ dataset-schema.py
@@ -17,22 +17,36 @@ class ColumnTypes(str, Enum):
     time = "time"
 
 
+class Grounding(BaseModel):
+    identifiers: dict[str, str] = Field(
+        description="",
+    )
+    context: Optional[dict[str, Any]] = Field(
+        description="",
+    )
+
+
 class DatasetColumn(BaseModel):
     name: str = Field(
         title="Name",
-        description="Name of column"
+        description="Name of column",
     )
     data_type: ColumnTypes = Field(
         default=ColumnTypes.unknown,
-        description=f"Datatype. One of: {', '.join(ColumnTypes)}"
+        description=f"Datatype. One of: {', '.join(ColumnTypes)}",
     )
     format_str: Optional[str] = Field(
-        description="String that describes the formatting of the value."
+        description="String that describes the formatting of the value.",
     )
     # format_str_type
     annotations: list[str] = Field(
+        description="",
     )
     metadata: dict[str, Any] = Field(
+        description="",
+    )
+    grounding: Optional[Grounding] = Field(
+        description="",
     )
 
 
@@ -40,14 +54,22 @@ class DatasetDocument(BaseModel):
     name: str = Field(
     )
     description: Optional[str] = Field(
+        description="Texual description of the dataset.",
     )
     data_url: Optional[AnyUrl] = Field(
+        description="Url from which the dataset can be downloaded/fetched.",
     )
     columns: list[DatasetColumn] = Field(
+        description="Information regarding the columns that make up the dataset.",
     )
-    metatada: dict[str, Any] = Field(
+    metatada: Optional[dict[str, Any]] = Field(
+        description="(Optional) Unformatted metadata about the dataset.",
     )
     source: Optional[str] = Field(
+        description="Source of dataset",
+    )
+    grounding: Optional[Grounding] = Field(
+        description="",
     )
 
 
