14ce825928780639f5e8e26bd4bf52c8a9ebdc3c
Todd Roper <toddroper@me.com>
2023-05-18 10:52:59 -0700

    Updated model to use model configs.

--- tds/modules/model/model.py
+++ tds/modules/model/model.py
@@ -9,6 +9,7 @@ from tds.db.base import TdsModel
 from tds.db.graph.neo4j import ENGINE
 from tds.db.relational import engine as pg_engine
 from tds.lib.concepts import mark_concept_active
+from tds.lib.model_configs import model_config
 from tds.schema.provenance import Provenance
 from tds.settings import settings
 
@@ -95,58 +96,4 @@ class Model(TdsModel):
         provenance_handler.create_entry(payload)
 
     class Config:
-        schema_extra = {
-            "example": {
-                "name": "A Test Model",
-                "schema": "https://raw.githubusercontent.com/DARPA-ASKEM/Model-Representations/petrinet_v0.2/petrinet/petrinet_schema.json",
-                "description": "Test Model Post from Swagger.",
-                "model_version": "1.0",
-                "model": {
-                    "states": [
-                        {
-                            "id": "S",
-                            "name": "S",
-                            "grounding": {
-                                "identifiers": {"ncbitaxon": "9606"},
-                                "context": {},
-                            },
-                        },
-                        {
-                            "id": "V",
-                            "name": "V",
-                            "grounding": {
-                                "identifiers": {"ncbitaxon": "9606"},
-                                "context": {},
-                            },
-                        },
-                    ],
-                    "transitions": [
-                        {
-                            "id": "t1",
-                            "input": ["S"],
-                            "output": [],
-                            "properties": {
-                                "name": "t1",
-                                "rate": {
-                                    "expression": "S/l_e",
-                                    "expression_mathml": "<apply><divide/><ci>S</ci><ci>l_e</ci></apply>",
-                                },
-                            },
-                        },
-                        {
-                            "id": "t2",
-                            "input": ["V"],
-                            "output": [],
-                            "properties": {
-                                "name": "t2",
-                                "rate": {
-                                    "expression": "V/l_e",
-                                    "expression_mathml": "<apply><divide/><ci>V</ci><ci>l_e</ci></apply>",
-                                },
-                            },
-                        },
-                    ],
-                    "parameters": [{"id": "l_e", "value": 50.0}],
-                },
-            }
-        }
+        schema_extra = {"example": model_config}
