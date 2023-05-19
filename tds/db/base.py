96f9e10313373dfcdc00c53eb00da31f336bd6ad
Todd Roper <toddroper@me.com>
2023-05-19 13:35:24 -0700

    Updated index prop to accessible.

--- tds/db/base.py
+++ tds/db/base.py
@@ -8,22 +8,22 @@ from tds.db import es
 
 class TdsModel(BaseModel):
     id: Optional[int | str]
-    _index: str
+    index: str
     timestamp: Optional[datetime]
 
     def save(self, entity_id: Optional[None | str | int] = None):
         self.timestamp = datetime.now()
         if self.id or entity_id:
             res = es.index(
-                index=self._index,
+                index=self.index,
                 body=self.dict(),
                 id=(entity_id if entity_id else self.id),
             )
         else:
-            res = es.index(index=self._index, body=self.dict())
+            res = es.index(index=self.index, body=self.dict())
             self.id = res["_id"]
         return res
 
     def delete(self):
-        res = es.delete(index=self._index, id=self.id)
+        res = es.delete(index=self.index, id=self.id)
         return res
