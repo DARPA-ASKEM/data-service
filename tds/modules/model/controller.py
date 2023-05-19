2781bdec9a6f834aada3be9f8814ccf916cef5a9
Todd Roper <toddroper@me.com>
2023-05-19 09:57:59 -0700

    First pass at refactoring model controller to follow FastAPI response convention.

--- tds/modules/model/controller.py
+++ tds/modules/model/controller.py
@@ -7,6 +7,7 @@ from typing import List
 
 from elasticsearch import NotFoundError
 from fastapi import APIRouter, Response, status
+from fastapi.responses import JSONResponse
 
 from tds.db import es
 from tds.modules.model.model import Model
@@ -32,13 +33,9 @@ def list_models(page_size: int = 100, page: int = 0) -> List:
     res = es.search(index="model", **list_body)
     print(res)
 
-    list_body = (
-        model_list_response(res["hits"]["hits"])
-        if res["hits"]["hits"]
-        else json.dumps([])
-    )
+    list_body = model_list_response(res["hits"]["hits"]) if res["hits"]["hits"] else []
 
-    return Response(
+    return JSONResponse(
         status_code=status.HTTP_200_OK,
         headers={
             "content-type": "application/json",
@@ -48,23 +45,23 @@ def list_models(page_size: int = 100, page: int = 0) -> List:
 
 
 @model_router.post("", **create.fastapi_endpoint_config)
-def model_post(payload: Model) -> Response:
+def model_post(payload: Model) -> JSONResponse:
     """
     Create model and return its ID
     """
     res = payload.save()
     logger.info("new model created: %s", res["_id"])
-    return Response(
+    return JSONResponse(
         status_code=200,
         headers={
             "content-type": "application/json",
         },
-        content=json.dumps({"id": res["_id"]}),
+        content={"id": res["_id"]},
     )
 
 
 @model_router.get("/{model_id}/descriptions", **retrieve.fastapi_endpoint_config)
-def model_descriptions_get(model_id: str | int) -> Response:
+def model_descriptions_get(model_id: str | int) -> JSONResponse | Response:
     """
     Retrieve a model 'description' from ElasticSearch
     """
@@ -72,14 +69,12 @@ def model_descriptions_get(model_id: str | int) -> Response:
         res = es.get(index="model", id=model_id)
         logger.info("model retrieved for description: %s", model_id)
 
-        return Response(
+        return JSONResponse(
             status_code=status.HTTP_200_OK,
             headers={
                 "content-type": "application/json",
             },
-            content=json.dumps(
-                model_response(res, delete_fields=["model", "model_version"])
-            ),
+            content=model_response(res, delete_fields=["model", "model_version"]),
         )
     except NotFoundError:
         return Response(
@@ -91,7 +86,7 @@ def model_descriptions_get(model_id: str | int) -> Response:
 
 
 @model_router.get("/{model_id}/parameters", **retrieve.fastapi_endpoint_config)
-def model_parameters_get(model_id: str | int) -> Response:
+def model_parameters_get(model_id: str | int) -> JSONResponse | Response:
     """
     Function retrieves a Model's parameters.
     """
@@ -99,12 +94,12 @@ def model_parameters_get(model_id: str | int) -> Response:
         res = es.get(index="model", id=model_id, source_includes=["model.parameters"])
         logger.info("model retrieved for params: %s", model_id)
 
-        return Response(
+        return JSONResponse(
             status_code=status.HTTP_200_OK,
             headers={
                 "content-type": "application/json",
             },
-            content=json.dumps(res["_source"]["model"]["parameters"]),
+            content=res["_source"]["model"]["parameters"],
         )
     except NotFoundError:
         return Response(
@@ -116,7 +111,7 @@ def model_parameters_get(model_id: str | int) -> Response:
 
 
 @model_router.get("/{model_id}", **retrieve.fastapi_endpoint_config)
-def model_get(model_id: str | int) -> Response:
+def model_get(model_id: str | int) -> JSONResponse | Response:
     """
     Retrieve a model from ElasticSearch
     """
@@ -124,12 +119,12 @@ def model_get(model_id: str | int) -> Response:
         res = es.get(index="model", id=model_id)
         logger.info("model retrieved: %s", model_id)
 
-        return Response(
+        return JSONResponse(
             status_code=status.HTTP_200_OK,
             headers={
                 "content-type": "application/json",
             },
-            content=json.dumps(model_response(res)),
+            content=model_response(res),
         )
     except NotFoundError:
         return Response(
@@ -141,18 +136,18 @@ def model_get(model_id: str | int) -> Response:
 
 
 @model_router.put("/{model_id}", **update.fastapi_endpoint_config)
-def model_put(model_id: str | int, payload: Model) -> Response:
+def model_put(model_id: str | int, payload: Model) -> JSONResponse:
     """
     Update a model in ElasticSearch
     """
     res = payload.save(model_id)
     logger.info("model updated: %s", model_id)
-    return Response(
+    return JSONResponse(
         status_code=status.HTTP_200_OK,
         headers={
             "content-type": "application/json",
         },
-        content=json.dumps({"id": res["_id"]}),
+        content={"id": res["_id"]},
     )
 
 
