de3500fd883bac0e98fc23617bd27ca4539a0cbb
Todd Roper <toddroper@me.com>
2023-06-01 15:07:36 -0700

    Updated workflow controller to use response.

--- tds/modules/workflow/controller.py
+++ tds/modules/workflow/controller.py
@@ -7,10 +7,12 @@ from logging import Logger
 
 from elasticsearch import NotFoundError
 from fastapi import APIRouter, Response, status
+from fastapi.encoders import jsonable_encoder
 from fastapi.responses import JSONResponse
 
 from tds.db import es_client
 from tds.modules.workflow.model import Workflow
+from tds.modules.workflow.response import WorkflowResponse, workflow_response
 from tds.operation import create, delete, retrieve, update
 
 workflow_router = APIRouter()
@@ -18,13 +20,15 @@ logger = Logger(__name__)
 es_index = Workflow.index
 
 
-@workflow_router.get("", **retrieve.fastapi_endpoint_config)
+@workflow_router.get(
+    "", response_model=list[WorkflowResponse], **retrieve.fastapi_endpoint_config
+)
 def list_workflows(page_size: int = 100, page: int = 0) -> JSONResponse:
     """
     Retrieve the list of workflows from ES.
     """
     es = es_client()
-    list_body = {"size": page_size, "source": False, "from_": page}
+    list_body = {"size": page_size, "from_": page}
 
     res = es.search(index=es_index, **list_body)
 
@@ -33,7 +37,7 @@ def list_workflows(page_size: int = 100, page: int = 0) -> JSONResponse:
         headers={
             "content-type": "application/json",
         },
-        content=res["hits"]["hits"],
+        content=jsonable_encoder(workflow_response(res["hits"]["hits"])),
     )
 
 
@@ -45,7 +49,7 @@ def workflow_post(payload: Workflow) -> JSONResponse:
     res = payload.save()
     logger.info("New workflow created: %s", res["_id"])
     return JSONResponse(
-        status_code=200,
+        status_code=status.HTTP_200_OK,
         headers={
             "content-type": "application/json",
         },
@@ -53,8 +57,12 @@ def workflow_post(payload: Workflow) -> JSONResponse:
     )
 
 
-@workflow_router.get("/workflow_id}", **retrieve.fastapi_endpoint_config)
-def workflow_get(workflow_id: str | int) -> JSONResponse | Response:
+@workflow_router.get(
+    "/{workflow_id}",
+    response_model=WorkflowResponse,
+    **retrieve.fastapi_endpoint_config,
+)
+def workflow_get(workflow_id: str) -> JSONResponse | Response:
     """
     Retrieve a workflow from ElasticSearch
     """
@@ -68,19 +76,19 @@ def workflow_get(workflow_id: str | int) -> JSONResponse | Response:
             headers={
                 "content-type": "application/json",
             },
-            content=res,
+            content=jsonable_encoder(res["_source"]),
         )
     except NotFoundError:
         return Response(status_code=status.HTTP_404_NOT_FOUND)
 
 
 @workflow_router.put("/{workflow_id}", **update.fastapi_endpoint_config)
-def workflow_put(workflow_id: str | int, payload: Workflow) -> JSONResponse | Response:
+def workflow_put(workflow_id: str, payload: Workflow) -> JSONResponse | Response:
     """
     Update a workflow in ElasticSearch
     """
     try:
-        res = payload.save(workflow_id)
+        res = payload.save()
         logger.info("workflow updated: %s", res["_id"])
         return JSONResponse(
             status_code=status.HTTP_200_OK,
@@ -99,7 +107,7 @@ def workflow_put(workflow_id: str | int, payload: Workflow) -> JSONResponse | Re
 
 
 @workflow_router.delete("/{workflow_id}", **delete.fastapi_endpoint_config)
-def workflow_delete(workflow_id: str | int) -> Response:
+def workflow_delete(workflow_id: str) -> Response:
     """
     Delete a Workflow in ElasticSearch
     """
