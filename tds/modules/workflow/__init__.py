d013acaaf347480deec8a72aca61295d5ac830f9
Todd Roper <toddroper@me.com>
2023-06-01 06:28:20 -0700

    Adding simulation workflow module.

---
+++ tds/modules/workflow/__init__.py
@@ -0,0 +1,10 @@
+"""
+    TDS Workflow Module.
+
+    A simple REST module that creates the basic endpoints for an entity in
+    Terarium Data Service (TDS).
+"""
+from tds.modules.workflow.controller import workflow_router as router
+
+ROUTE_PREFIX = "workflows"
+TAGS = ["Workflow"]
