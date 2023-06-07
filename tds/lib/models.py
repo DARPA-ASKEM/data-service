e0948a9e44410be9776b30f0c1505d89c857d2f0
Todd Roper <toddroper@me.com>
2023-06-07 15:49:21 -0700

    Revert "Removing model lib, no longer used."
    This reverts commit 8ab2fd358569a3055fde1f5467f314e6f1ba6911.

---
+++ tds/lib/models.py
@@ -0,0 +1,64 @@
+"""
+Model-specific logic
+"""
+
+from logging import Logger
+
+from sqlalchemy.orm import Session
+
+from tds.autogen import orm
+from tds.autogen.enums import ModelOperations
+from tds.schema.model import ModelParameters
+
+logger = Logger(__file__)
+
+model_opt_relationship_mapping = {
+    ModelOperations.copy: orm.RelationType.COPIED_FROM,
+    ModelOperations.decompose: orm.RelationType.DECOMPOSED_FROM,
+    ModelOperations.stratify: orm.RelationType.STRATIFIED_FROM,
+    ModelOperations.glue: orm.RelationType.GLUED_FROM,
+}
+
+
+def adjust_model_params(model_id: int, parameters: ModelParameters, session: Session):
+    """
+    Add new entries and remove unused entries
+    """
+
+    existing = []
+    for param in session.query(orm.ModelParameter).filter(
+        orm.ModelParameter.model_id == model_id
+    ):
+        existing.append(param.get("name"))
+
+    for parameter in parameters:
+        if parameter.get("name") not in existing:
+            session.add(
+                orm.ModelParameter(
+                    model_id=model_id,
+                    name=parameter.get("name"),
+                    type=parameter.get("type"),
+                    default_value=parameter.get("default_value"),
+                    state_variable=parameter.get("state_variable", False),
+                )
+            )
+        else:
+            session.query(orm.ModelParameter).filter(
+                orm.ModelParameter.model_id == model_id,
+                orm.ModelParameter.id == parameter.get("id"),
+            ).update(
+                {
+                    "type": parameter.get("type"),
+                    "default_value": parameter.get("default_value"),
+                    "state_variable": parameter.get("state_variable", False),
+                }
+            )
+
+            existing.remove(parameter.name)
+
+    for name in existing:
+        for param in session.query(orm.ModelParameter).filter(
+            orm.ModelParameter.model_id == model_id,
+            orm.ModelParameter.name == name,
+        ):
+            session.delete(param)
