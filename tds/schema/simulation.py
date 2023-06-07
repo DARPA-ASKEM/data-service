e6bfa7ebe416f4686327cb2b944e2f775d9d0e87
Todd Roper <toddroper@me.com>
2023-06-07 13:48:15 -0700

    Cleaned out old simulation pieces.

--- tds/schema/simulation.py
+++ tds/schema/simulation.py
@@ -7,14 +7,14 @@ from json import dumps
 from typing import List, Optional
 
 from tds.autogen import orm
-from tds.autogen.schema import SimulationRun
 from tds.modules.model_configuration.model import ModelConfiguration
+from tds.modules.simulation.model import Simulation
 from tds.schema.concept import Concept
 
 SimulationParameters = List[dict]
 
 
-def orm_to_params(parameters: List[orm.SimulationParameter]) -> SimulationParameters:
+def orm_to_params(parameters: List) -> SimulationParameters:
     """
     Convert SQL parameter search to dict
     """
@@ -22,54 +22,3 @@ def orm_to_params(parameters: List[orm.SimulationParameter]) -> SimulationParame
         {"name": param.name, "value": param.value, "type": param.type, "id": param.id}
         for param in parameters
     ]
-
-
-class Plan(ModelConfiguration):
-    concept: Optional[Concept] = None
-
-    @classmethod
-    def from_orm(cls, body: orm.SimulationRun) -> "Plan":
-        """
-        Handle ORM conversion while coercing `dict` to JSON
-        """
-        body.__dict__["content"] = dumps(body.content)
-        return super().from_orm(body)
-
-    class Config:
-        orm_mode = True
-        schema_extra = {
-            "example": {
-                "name": "string",
-                "simulator": "string",
-                "model_id": "int",
-                "dataset_id": "int",
-                "query": "string",
-                "content": "json-in-string",
-            }
-        }
-
-
-class RunDescription(SimulationRun):
-    class Config:
-        orm_mode = True
-
-
-class Run(SimulationRun):
-    parameters: SimulationParameters = {}
-
-    @classmethod
-    def from_orm(
-        cls, body: orm.SimulationRun, parameters: List[orm.SimulationParameter]
-    ) -> "Run":
-        """
-        Handle ORM conversion while including parameters
-        """
-        setattr(
-            body,
-            "parameters",
-            orm_to_params(parameters),
-        )
-        return super().from_orm(body)
-
-    class Config:
-        orm_mode = True
