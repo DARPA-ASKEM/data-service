7c8c32f8c943cb92fcac81b5528fc970e163ce08
Todd Roper <toddroper@me.com>
2023-06-09 16:23:31 -0700

    Removed es heartbeat.

--- tds/db/elasticsearch.py
+++ tds/db/elasticsearch.py
@@ -2,11 +2,8 @@
 The configured ElasticSearch DB engine
 """
 import logging
-import time
 
-from elastic_transport import ConnectionError as EsConnectionError
 from elasticsearch import Elasticsearch
-from elasticsearch.client import ClusterClient
 
 from tds.settings import settings
 
@@ -20,32 +17,3 @@ def es_client():
     return Elasticsearch(
         [settings.ES_URL], basic_auth=(settings.ES_USERNAME, settings.ES_PASSWORD)
     )
-
-
-def wait_for_es_up(
-    client=None, timeout=120, sleep=5, healthy_statuses=("yellow", "green")
-):
-    """
-    Function provides a healthcheck for ElasticSearch.
-    """
-    # Use default client if not provided
-    if client is None:
-        client = es_client()
-    cluster_client = ClusterClient(client)
-
-    # Wait for cluster to become healthy enough to create indexes
-    healthy = False
-    start = time.time()
-    logger.debug("ES wait for up started at %s", start)
-    es_logger = logging.getLogger("elastic_transport")
-    original_es_logging_level = es_logger.level
-    es_logger.setLevel(logging.ERROR)
-    while not (healthy or (time.time() - start) > timeout):
-        try:
-            health = cluster_client.health()
-            healthy = health["status"] in healthy_statuses
-        except EsConnectionError:
-            logger.warning("ElasticSearch is not ready. Sleeping %s seconds", sleep)
-            time.sleep(sleep)
-    es_logger.setLevel(original_es_logging_level)
-    logger.debug("ES wait finished at %s", time.time())
