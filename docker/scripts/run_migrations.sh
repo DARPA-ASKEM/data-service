782d0252faa59c4a41dfd63ef16bb5ad5d2d81ef
Todd Roper <toddroper@me.com>
2023-06-09 16:37:23 -0700

    Improved messages in migration script.

--- docker/scripts/run_migrations.sh
+++ docker/scripts/run_migrations.sh
@@ -1,17 +1,17 @@
 #!/usr/bin/env bash
 is_up_to_date=$(alembic -c migrate/alembic.ini check)
 
+echo "Beginning migration check."
 if [[ $is_up_to_date == *"Target database is not up to date."* ]] ; then
   echo "Running migrations."
   alembic -c migrate/alembic.ini upgrade head
+  echo "Completed Postgres migrations."
+else
+  echo "DB up to date."
 fi
-echo "Completed Postgres migrations."
-
 
 echo "Running Elasticsearch index build"
-
 python migrate/start_elasticsearch.py
-
 
 if [[ "$SEED_DATA" == "true" ]] ; then
   echo "Seeding Data"
