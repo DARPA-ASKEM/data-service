53260de991319650eaf9f16d27aecdb483ca173c
Todd Roper <toddroper@me.com>
2023-06-09 16:16:55 -0700

    Updated migration env handling.

--- migrate/env.py
+++ migrate/env.py
@@ -9,21 +9,38 @@ The environment file provides the necessary context for the migrations
 
 Source: 'https://alembic.sqlalchemy.org/en/latest/tutorial.html'
 """
+import os
+import re
 from logging.config import fileConfig
 
 # pylint: disable=no-member
 # NOTE: context doesn't exist until runtime
 from alembic import context
-from sqlalchemy import engine_from_config, pool
-
-from tds.autogen.orm import Base
+from sqlalchemy import create_engine, engine_from_config, pool
 
 config = context.config
 
 if config.config_file_name is not None:
     fileConfig(config.config_file_name)
 
-target_metadata = Base.metadata
+
+def setup_context():
+    tokens = {
+        "SQL_USER": os.getenv("SQL_USER"),
+        "SQL_PASSWORD": os.getenv("SQL_PASSWORD"),
+        "SQL_URL": os.getenv("SQL_URL"),
+        "SQL_PORT": str(os.getenv("SQL_PORT")),
+        "SQL_DB": os.getenv("SQL_DB"),
+    }
+    url = config.get_main_option("sqlalchemy.url")
+    url = re.sub(r"\${(.+?)}", lambda m: tokens[m.group(1)], url)
+
+    context.configure(
+        url=url,
+        dialect_opts={"paramstyle": "named"},
+    )
+
+    return url
 
 
 def run_migrations_offline() -> None:
@@ -38,13 +55,7 @@ def run_migrations_offline() -> None:
     script output.
 
     """
-    url = config.get_main_option("sqlalchemy.url")
-    context.configure(
-        url=url,
-        target_metadata=target_metadata,
-        literal_binds=True,
-        dialect_opts={"paramstyle": "named"},
-    )
+    url = setup_context()
 
     with context.begin_transaction():
         context.run_migrations()
@@ -57,19 +68,15 @@ def run_migrations_online() -> None:
     and associate a connection with the context.
 
     """
+    url = setup_context()
     connectable = context.config.attributes.get("connection", None)
 
     if connectable is None:
-        connectable = engine_from_config(
-            context.config.get_section(context.config.config_ini_section),
-            prefix="sqlalchemy.",
-            poolclass=pool.NullPool,
-        )
+        connectable = create_engine(url)
 
     with connectable.connect() as connection:
         context.configure(
             connection=connection,
-            target_metadata=target_metadata,
             compare_type=True,
             compare_server_default=True,
             include_schemas=True,
