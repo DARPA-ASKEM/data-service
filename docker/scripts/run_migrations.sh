aa218d2982d0bace3e4c970664947b334d870729
Todd Roper <toddroper@me.com>
2023-06-09 17:49:22 -0700

    Updated migration flow.

--- docker/scripts/run_migrations.sh
+++ docker/scripts/run_migrations.sh
@@ -1,14 +1,9 @@
 #!/usr/bin/env bash
-is_up_to_date=$(alembic -c migrate/alembic.ini check)
 
-echo "Beginning migration check."
-if [[ $is_up_to_date == *"Target database is not up to date."* ]] ; then
-  echo "Running migrations."
-  alembic -c migrate/alembic.ini upgrade head
-  echo "Completed Postgres migrations."
-else
-  echo "DB up to date."
-fi
+
+echo "Running migrations."
+alembic -c migrate/alembic.ini upgrade head
+echo "Completed Postgres migrations."
 
 echo "Running Elasticsearch index build"
 python migrate/start_elasticsearch.py
@@ -16,7 +11,7 @@ python migrate/start_elasticsearch.py
 if [[ "$SEED_DATA" == "true" ]] ; then
   echo "Seeding Data"
 fi
-touch /logger.log
-tail -f /logger.log
+#touch /logger.log
+#tail -f /logger.log
 echo "Booting TDS API."
 exit 0
