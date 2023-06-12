15510f6e6ed0749d112b1dcd28227336e9da8cd1
Todd Roper <toddroper@me.com>
2023-06-12 12:34:50 -0700

    Adding merged es script.

--- migrate/scripts/start_elasticsearch.py
+++ migrate/scripts/start_elasticsearch.py
@@ -1,20 +1,26 @@
 #!/usr/bin/env python
 import json
 import os
+import sys
+from pathlib import Path
 
-from elasticsearch import Elasticsearch
+from elasticsearch import ConflictError, Elasticsearch
 
 ES_URL = os.getenv("ES_URL")
 ES_USERNAME = os.getenv("ES_USERNAME")
 ES_PASSWORD = os.getenv("ES_PASSWORD")
 ES_INDEX_PREFIX = os.getenv("ES_INDEX_PREFIX")
 
+migrate_dir = Path(os.path.dirname(__file__))
+seed_dir = f"{migrate_dir.parent}/seeds"
+es_seed_dir = f"{seed_dir}/es"
+es = Elasticsearch([ES_URL], basic_auth=(ES_USERNAME, ES_PASSWORD))
+
 
 def setup_elasticsearch_indexes() -> None:
     """
     Function creates indexes in ElasticSearch.
     """
-    es = Elasticsearch([ES_URL], basic_auth=(ES_USERNAME, ES_PASSWORD))
     # Config should match keyword args on
     # https://elasticsearch-py.readthedocs.io/en/v8.3.2/api.html#elasticsearch.client.IndicesClient.create
     with open("/migrate/seeds/indexes.json", "r") as index_file:
@@ -30,6 +36,34 @@ def setup_elasticsearch_indexes() -> None:
                 es.indices.create(index=index_name, mappings=config)
 
 
+def save_data_to_es(directory, data):
+    index = f"{ES_INDEX_PREFIX}{directory}"
+    data_dict = json.load(data)
+    try:
+        res = es.create(index=index, document=data_dict, id=data_dict["id"])
+        if res["result"] != "created":
+            print(res)
+        else:
+            name = data_dict["name"]
+            print(f"Created {name}.")
+    except ConflictError as e:
+        fail_id = data_dict["id"]
+        print(f"Item with id {fail_id} already exists in {index}. ({e.status_code})")
+
+
+def seed_es_data():
+    print("Seeding ElasticSearch Data.")
+    for directory in os.listdir(es_seed_dir):
+        path_dir = f"{es_seed_dir}/{directory}"
+        for file in os.listdir(path_dir):
+            file_path = f"{path_dir}/{file}"
+            with open(file_path, "r") as es_file_data:
+                save_data_to_es(directory=directory, data=es_file_data)
+
+
 if __name__ == "__main__":
-    print("Checking Elastic Search.")
-    setup_elasticsearch_indexes()
+    if len(sys.argv) > 1:
+        seed_es_data()
+    else:
+        print("Checking Elastic Search.")
+        setup_elasticsearch_indexes()
