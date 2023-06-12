29a5f75bd20f2c05d3b91a6a5ddd69db8fce8201
Todd Roper <toddroper@me.com>
2023-06-12 16:29:20 -0700

    Fixed field naming error.

--- migrate/versions/1f5853959c65_init_db.py
+++ migrate/versions/1f5853959c65_init_db.py
@@ -102,7 +102,7 @@ def upgrade() -> None:
         sa.Column("name", sa.String(), nullable=False),
         sa.Column("version", sa.String(), nullable=False),
         sa.Column("semantics", sa.String(), nullable=False),
-        sa.Column("url_schema", sa.String(), nullable=False),
+        sa.Column("schema_url", sa.String(), nullable=False),
         sa.PrimaryKeyConstraint("name"),
     )
     op.create_table(
