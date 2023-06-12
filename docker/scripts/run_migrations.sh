843aef09d019635b5ec4a7d04e66083b5ddab7b5
Todd Roper <toddroper@me.com>
2023-06-12 09:57:32 -0700

    Added TS instruction to migration entrypoint.

--- docker/scripts/run_migrations.sh
+++ docker/scripts/run_migrations.sh
@@ -12,6 +12,7 @@ if [[ "$SEED_DATA" == "true" ]] ; then
   echo "Seeding Data"
   python migrate/seed_data.py
 fi
+# To Troubleshoot the migration container, uncomment the following lines:
 #touch /logger.log
 #tail -f /logger.log
 echo "Booting TDS API."
