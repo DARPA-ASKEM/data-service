a14ed27fbfabe73cf53a2e659217b41af5bf24ac
Todd Roper <toddroper@me.com>
2023-06-12 11:35:36 -0700

    Updated entry to use new scripts dir.

--- docker/scripts/run_migrations.sh
+++ docker/scripts/run_migrations.sh
@@ -5,15 +5,18 @@ echo "Running migrations."
 alembic -c migrate/alembic.ini upgrade head
 echo "Completed Postgres migrations."
 
+echo "Checking storage bucket."
+
+
 echo "Running Elasticsearch index build"
-python migrate/start_elasticsearch.py
+python migrate/scripts/start_elasticsearch.py
 
 if [[ "$SEED_DATA" == "true" ]] ; then
   echo "Seeding Data"
-  python migrate/seed_data.py
+  python migrate/scripts/seed_data.py
 fi
 # To Troubleshoot the migration container, uncomment the following lines:
-#touch /logger.log
-#tail -f /logger.log
+touch /logger.log
+tail -f /logger.log
 echo "Booting TDS API."
 exit 0
