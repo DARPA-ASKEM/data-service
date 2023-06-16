b7e11eae905a12faae6e4375aaa3827469ed4146
Todd Roper <toddroper@me.com>
2023-06-16 13:58:29 -0700

    Moved helpers to provenance module.

--- tds/db/helpers.py
+++ tds/db/helpers.py
@@ -10,27 +10,6 @@ from sqlalchemy.orm import Session
 from tds.autogen import orm
 
 
-def return_graph_validations():
-    """
-    read in graph relation file
-    """
-    with open("graph_relations.json", "r", encoding="utf-8") as file:
-        validation = json.load(file)
-    return validation
-
-
-def validate_relationship(left, right, relation_type):
-    """
-    validate a relationship for provenance
-    """
-    validations = return_graph_validations()
-    relationship_allowed_types = validations[relation_type]
-    for relation in relationship_allowed_types:
-        if left == relation[0] and right == relation[1]:
-            return True
-    return False
-
-
 def init_dev_content(connection: Connection):
     """
     Initialize tables in the connected DB
