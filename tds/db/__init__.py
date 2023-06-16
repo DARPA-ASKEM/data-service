67c6b66f5dfcf671d24c520b4141c3358b69105f
Todd Roper <toddroper@me.com>
2023-06-16 13:56:58 -0700

    Removing provenance from db init to prevent circular import.

--- tds/db/__init__.py
+++ tds/db/__init__.py
@@ -4,8 +4,6 @@ tds.db - DB handling
 
 from tds.db.elasticsearch import es_client
 from tds.db.graph.neo4j import request_engine as request_graph_db
-from tds.db.graph.provenance_handler import ProvenanceHandler
-from tds.db.graph.search_provenance import SearchProvenance
 from tds.db.helpers import drop_content, entry_exists, init_dev_content, list_by_id
 from tds.db.relational import engine as rdb
 from tds.db.relational import request_engine as request_rdb
