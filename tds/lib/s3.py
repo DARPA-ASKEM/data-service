3055a997e1fd8bccd8d1db94b04e88a67998f3a6
Todd Roper <toddroper@me.com>
2023-06-21 09:47:24 -0700

    Updated s3 client to unify common vars.

--- tds/lib/s3.py
+++ tds/lib/s3.py
@@ -12,41 +12,43 @@ def s3_client():
     """
     Function sets up an S3 client based on env settings.
     """
+    s3_opts = {
+        "aws_access_key_id": settings.AWS_ACCESS_KEY_ID,
+        "aws_secret_access_key": settings.AWS_SECRET_ACCESS_KEY,
+        "config": boto3.session.Config(signature_version="s3v4"),
+    }
     if settings.STORAGE_HOST:
-        s3_ = boto3.client(
-            "s3",
-            endpoint_url=settings.STORAGE_HOST,
-            aws_access_key_id=settings.AWS_ACCESS_KEY_ID,
-            aws_secret_access_key=settings.AWS_SECRET_ACCESS_KEY,
-            aws_session_token=None,
-            config=boto3.session.Config(signature_version="s3v4"),
-            verify=False,
-        )
+        s3_opts["endpoint_url"] = settings.STORAGE_HOST
+        s3_opts["aws_session_token"] = None
+        s3_opts["verify"] = False
     else:
-        s3_ = boto3.client(
-            "s3",
-            region_name=settings.AWS_REGION,
-            aws_access_key_id=settings.AWS_ACCESS_KEY_ID,
-            aws_secret_access_key=settings.AWS_SECRET_ACCESS_KEY,
-            config=boto3.session.Config(signature_version="s3v4"),
-        )
+        s3_opts["region_name"] = settings.AWS_REGION
+
+    s3_ = boto3.client("s3", **s3_opts)
 
     return s3_
 
 
-def get_file_path(entity_id: str | int, file_name: str) -> str:
+def get_file_path(
+    entity_id: str | int, file_name: str, path: str = settings.S3_RESULTS_PATH
+) -> str:
     """
     Function builds a file path for s3.
     """
-    return os.path.join(settings.S3_RESULTS_PATH, str(entity_id), file_name)
+    return os.path.join(path, str(entity_id), file_name)
 
 
-def get_presigned_url(entity_id: str | int, file_name: str, method: str):
+def get_presigned_url(
+    entity_id: str | int,
+    file_name: str,
+    method: str,
+    path: str = settings.S3_RESULTS_PATH,
+):
     """
     Function generates a presigned URL for the HMI client.
     """
     s3_ = s3_client()
-    s3_key = get_file_path(entity_id, file_name)
+    s3_key = get_file_path(entity_id=entity_id, file_name=file_name, path=path)
 
     presigned_url = s3_.generate_presigned_url(
         ClientMethod=method,
