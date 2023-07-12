a943c9a741d4aa46385c77291b56f32fd793a73b
Todd Roper <toddroper@me.com>
2023-07-12 08:57:25 -0700

    Removed QualifierXref and external classes from ORM.

--- tds/autogen/orm.py
+++ tds/autogen/orm.py
@@ -13,16 +13,6 @@ from tds.autogen.enums import ExtractedType, ValueType
 Base = declarative_base()
 
 
-class QualifierXref(Base):
-    __tablename__ = "qualifier_xref"
-
-    id = sa.Column(sa.Integer(), primary_key=True)
-    qualifier_id = sa.Column(
-        sa.Integer(), sa.ForeignKey("qualifier.id"), nullable=False
-    )
-    feature_id = sa.Column(sa.Integer(), sa.ForeignKey("feature.id"), nullable=False)
-
-
 class Feature(Base):
     __tablename__ = "feature"
 
@@ -44,18 +34,6 @@ class Qualifier(Base):
     value_type = sa.Column(sa.Enum(ValueType), nullable=False)
 
 
-class Extraction(Base):
-    __tablename__ = "extraction"
-
-    id = sa.Column(sa.Integer(), primary_key=True)
-    publication_id = sa.Column(
-        sa.Integer(), sa.ForeignKey("publication.id"), nullable=False
-    )
-    type = sa.Column(sa.Enum(ExtractedType), nullable=False)
-    data = sa.Column(sa.LargeBinary(), nullable=False)
-    img = sa.Column(sa.LargeBinary(), nullable=False)
-
-
 class ModelFramework(Base):
     __tablename__ = "model_framework"
 
@@ -63,20 +41,3 @@ class ModelFramework(Base):
     version = sa.Column(sa.String(), nullable=False)
     semantics = sa.Column(sa.String(), nullable=False)
     schema_url = sa.Column(sa.String())
-
-
-class Software(Base):
-    __tablename__ = "software"
-
-    id = sa.Column(sa.Integer(), primary_key=True)
-    timestamp = sa.Column(sa.DateTime(), nullable=False, server_default=func.now())
-    source = sa.Column(sa.String(), nullable=False)
-    storage_uri = sa.Column(sa.String(), nullable=False)
-
-
-class Publication(Base):
-    __tablename__ = "publication"
-
-    id = sa.Column(sa.Integer(), primary_key=True)
-    xdd_uri = sa.Column(sa.String(), nullable=False)
-    title = sa.Column(sa.String(), nullable=False)
