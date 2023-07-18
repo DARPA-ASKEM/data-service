3e535882a09923a8e12ef048b15658bd3f7fc5f8
Todd Roper <toddroper@me.com>
2023-07-18 11:32:41 -0700

    Linter work.

--- copy_project.py
+++ copy_project.py
@@ -1,4 +1,7 @@
 #! venv/bin/python
+"""
+Copy project CLI command.
+"""
 import argparse
 import json
 
@@ -6,10 +9,16 @@ import requests
 
 
 def extract_args(cli_args) -> tuple:
+    """
+    Function extracts args from cli command.
+    """
     return cli_args.id, cli_args.source, cli_args.destination
 
 
 def scrub_obj(obj: dict):
+    """
+    Function scrubs system items from object before copying.
+    """
     new_obj = {}
     for key in obj.keys():
         if key in ["id", "timestamp"]:
@@ -20,16 +29,21 @@ def scrub_obj(obj: dict):
 
 
 def post_to_destination(url: str, body: dict | None):
+    """
+    Function runs post request to url.
+    """
     print(f"Posting to {url}")
-    obj_create = requests.post(url=url, data=json.dumps(body) if body else None)
+    obj_create = requests.post(
+        url=url, data=json.dumps(body) if body else None, timeout=120
+    )
     return obj_create.json()
 
 
-def handle_file():
-    pass
-
-
 class CopyProject:
+    """
+    Class handles copying project between environments.
+    """
+
     asset_url = "{host}/projects/{project_id}/assets/{asset_type}/{asset_id}"
     download_url = "{host}/{asset_type}/{asset_id}/download-url"
     upload_url = "{host}/{asset_type}/{asset_id}/upload-url"
@@ -45,21 +59,30 @@ class CopyProject:
         self.destination_url = dest
 
     def fetch_project(self):
+        """
+        Function fetches project from origin.
+        """
         fetch_url = self.resource_url.format(
             host=self.source_url, resource_uri="projects", entity_id=self.project_id
         )
         print(f"Fetching project from: {fetch_url}")
-        project_fetch = requests.get(fetch_url)
+        project_fetch = requests.get(url=fetch_url, timeout=120)
         self.source_project = project_fetch.json()
         self.source_project_assets = self.source_project.pop("assets")
 
     def copy_project_obj(self):
+        """
+        Function copies base project to destination.
+        """
         new_project = scrub_obj(self.source_project)
         post_url = self.post_url.format(host=self.destination_url, resource="projects")
         response = post_to_destination(url=post_url, body=new_project)
         self.destination_project_id = response["id"]
 
     def process_assets(self):
+        """
+        Function processes project assets.
+        """
         for key in self.source_project_assets:
             if len(self.source_project_assets[key]):
                 resource_uri = key if key != "publications" else "external/publications"
@@ -67,7 +90,7 @@ class CopyProject:
                     asset_url = self.resource_url.format(
                         host=self.source_url, resource_uri=resource_uri, entity_id=asset
                     )
-                    asset_fetch = requests.get(asset_url)
+                    asset_fetch = requests.get(url=asset_url, timeout=120)
                     asset_json = asset_fetch.json()
                     asset_resp = self.post_asset(
                         asset=asset_json, asset_type=resource_uri
@@ -86,7 +109,6 @@ class CopyProject:
                             if key == "simulation"
                             else asset_json["file_names"]
                         )
-                        print(f"Processing file.")
                         source_download_url = self.download_url.format(
                             host=self.source_url,
                             asset_type=key,
@@ -99,23 +121,31 @@ class CopyProject:
                         )
                         for file in files:
                             download_url_request = requests.get(
-                                url=source_download_url, params={"filename": file}
+                                url=source_download_url,
+                                params={"filename": file},
+                                timeout=120,
                             )
                             download_response = download_url_request.json()
                             download_url = download_response["url"]
-                            download_file = requests.get(download_url)
+                            download_file = requests.get(url=download_url, timeout=120)
 
                             upload_url_request = requests.get(
-                                url=destination_upload_url, params={"filename": file}
+                                url=destination_upload_url,
+                                params={"filename": file},
+                                timeout=120,
                             )
                             upload_url = upload_url_request.json()
 
                             upload_file = requests.put(
                                 url=upload_url["url"],
                                 files={f"{file}": download_file.content},
+                                timeout=120,
                             )
 
     def post_asset(self, asset, asset_type):
+        """
+        Function posts asset to destination.
+        """
         post_url = self.post_url.format(host=self.destination_url, resource=asset_type)
 
         return post_to_destination(url=post_url, body=scrub_obj(asset))
