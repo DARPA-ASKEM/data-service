712593a993c8e30354998849b4ae6b2afbc230c3
Todd Roper <toddroper@me.com>
2023-08-08 12:35:51 -0700

    Refactored relational db for testing.

--- tds/db/relational.py
+++ tds/db/relational.py
@@ -7,10 +7,12 @@ from sqlalchemy import create_engine
 from tds.settings import settings
 
 # pylint: disable-next=line-too-long
-url = f"postgresql+psycopg2://{settings.SQL_USER}:{settings.SQL_PASSWORD}@{settings.SQL_URL}:{settings.SQL_PORT}/{settings.SQL_DB}"
-engine = create_engine(
-    url, pool_size=25, max_overflow=10, connect_args={"connect_timeout": 8}
-)
+sql_url = f"{settings.SQL_PROTOCOL}://{settings.SQL_USER}:{settings.SQL_PASSWORD}@{settings.SQL_URL}:{settings.SQL_PORT}/{settings.SQL_DB}"
+sql_args = {"pool_size": 25, "max_overflow": 10, "connect_args": {"connect_timeout": 8}}
+if settings.SQL_CONN_STR:
+    sql_url = settings.SQL_CONN_STR
+    sql_args = {}
+engine = create_engine(sql_url, **sql_args)
 
 
 async def request_engine():
