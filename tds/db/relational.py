2297952d2e836e3562f99c7f11651c48d9fc2392
Todd Roper <toddroper@me.com>
2023-08-08 14:43:16 -0700

    Fixing linter issue.

--- tds/db/relational.py
+++ tds/db/relational.py
@@ -7,12 +7,12 @@ from sqlalchemy import create_engine
 from tds.settings import settings
 
 # pylint: disable-next=line-too-long
-sql_url = f"{settings.SQL_PROTOCOL}://{settings.SQL_USER}:{settings.SQL_PASSWORD}@{settings.SQL_URL}:{settings.SQL_PORT}/{settings.SQL_DB}"
+SQL_URL = f"{settings.SQL_PROTOCOL}://{settings.SQL_USER}:{settings.SQL_PASSWORD}@{settings.SQL_URL}:{settings.SQL_PORT}/{settings.SQL_DB}"
 sql_args = {"pool_size": 25, "max_overflow": 10, "connect_args": {"connect_timeout": 8}}
 if settings.SQL_CONN_STR:
-    sql_url = settings.SQL_CONN_STR
+    SQL_URL = settings.SQL_CONN_STR
     sql_args = {}
-engine = create_engine(sql_url, **sql_args)
+engine = create_engine(SQL_URL, **sql_args)
 
 
 async def request_engine():
