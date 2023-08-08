ce1abef7231415bb945f9f5eddf27442779ca007
Todd Roper <toddroper@me.com>
2023-08-08 12:38:00 -0700

    Refactored artifact enum migration for testing.

--- migrate/versions/1a3cf96fb50f_add_artifact_to_enums.py
+++ migrate/versions/1a3cf96fb50f_add_artifact_to_enums.py
@@ -10,6 +10,13 @@ import sqlalchemy as sa
 # pylint: disable=no-member, invalid-name
 from alembic import op
 
+from migrate.scripts.enums import (
+    provenance_type,
+    resource_type,
+    taggable_type,
+    update_enum,
+)
+
 # revision identifiers, used by Alembic.
 revision = "1a3cf96fb50f"
 down_revision = "895deab7e80c"
@@ -18,10 +25,65 @@ depends_on = None
 
 
 def upgrade() -> None:
-    with op.get_context().autocommit_block():
-        op.execute("ALTER TYPE public.resourcetype ADD VALUE 'artifacts'")
-        op.execute("ALTER TYPE public.taggabletype ADD VALUE 'artifacts'")
-        op.execute("ALTER TYPE public.provenancetype ADD VALUE 'Artifact'")
+    resource_enums = {
+        "association": "resource_type",
+        "project_asset": "resource_type",
+    }
+    resource_list = [
+        "datasets",
+        "models",
+        "model_configurations",
+        "publications",
+        "simulations",
+        "workflows",
+        "artifacts",
+    ]
+    update_enum(
+        name="resourcetype",
+        enum_obj=resource_type,
+        cols=resource_enums,
+        enum_entities=resource_list,
+    )
+    taggable_enums = {
+        "ontology_concept": "type",
+    }
+    taggable_list = [
+        "datasets",
+        "models",
+        "projects",
+        "publications",
+        "qualifiers",
+        "simulation_parameters",
+        "model_configurations",
+        "simulations",
+        "workflows",
+        "artifacts",
+    ]
+    update_enum(
+        name="taggabletype",
+        enum_obj=taggable_type,
+        cols=taggable_enums,
+        enum_entities=taggable_list,
+    )
+    provenance_enums = {
+        "provenance": ["left_type", "right_type"],
+    }
+    provenance_list = [
+        "Concept",
+        "Dataset",
+        "Model",
+        "ModelConfiguration",
+        "Project",
+        "Publication",
+        "Simulation",
+        "Artifact",
+    ]
+    update_enum(
+        name="provenancetype",
+        enum_obj=provenance_type,
+        cols=provenance_enums,
+        enum_entities=provenance_list,
+    )
 
 
 def downgrade() -> None:
