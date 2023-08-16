9e7112cf643cc730379405899ab56a034bfd4ebd
Todd Roper <toddroper@me.com>
2023-08-16 06:14:54 -0700

    Updated model fields.

--- tds/modules/code/model.py
+++ tds/modules/code/model.py
@@ -1,11 +1,10 @@
 """
 TDS Code Data Model Definition.
 """
-from typing import Any, List, Optional
+from typing import Optional
 
 from tds.db.base import TdsModel
-
-# from tds.settings import settings
+from tds.db.enums import ProgrammingLanguage
 
 
 class Code(TdsModel):
@@ -15,25 +14,12 @@ class Code(TdsModel):
 
     name: str
     description: str
+    filename: str
+    repo_url: str
+    language: ProgrammingLanguage
+    metadata: Optional[dict]
 
     _index = "code"
-    concepts: Optional[List] = []
-
-    # def save(self):
-    #     """
-    #     Method saves the object to ElasticSearch.
-    #     """
-    #     res = super().save()
-    #     # self._extract_concepts()
-    #     if settings.NEO4J_ENABLED:
-    #         self._establish_provenance()
-    #     return res
-
-    # def _extract_concepts(self, artifact_id):
-    #     pass
-
-    # def _establish_provenance(self):
-    #     pass
 
     class Config:
         """
