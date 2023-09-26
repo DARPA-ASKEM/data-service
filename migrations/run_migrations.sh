b9ae0c7cf37888b7ffe85d60cba2a0bded9a1802
Matthew Printz <matt@jataware.com>
2023-09-26 14:52:34 -0500

    Cleanup

--- migrations/run_migrations.sh
+++ migrations/run_migrations.sh
@@ -3,21 +3,4 @@
 echo "Running migrations."
 alembic -c migrations/alembic.ini upgrade head 2>/dev/stdout
 echo "Migrations complete."
-
-# echo "Checking storage bucket."
-# python migrations/scripts/file_storage.py create_bucket
-
-# echo "Running Elasticsearch index build"
-# python migrations/scripts/start_elasticsearch.py
-
-# if [[ "$SEED_DATA" == "true" ]] ; then
-#   echo "Seeding Data"
-#   python migrations/scripts/start_elasticsearch.py "seed"
-#   python migrations/scripts/seed_data.py
-#   python migrations/scripts/file_storage.py upload_file datasets
-# fi
-# To Troubleshoot the migration container, uncomment the following lines:
-#touch /logger.log
-#tail -f /logger.log
-echo "Completed running migrations."
 exit 0
