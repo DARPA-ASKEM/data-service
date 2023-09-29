e05b07e90fdd062f883d2ff1ee7ea16d042067c6
Matthew Printz <matt@jataware.com>
2023-09-29 14:14:34 -0500

    Fix default RDB port

--- tds/settings.py
+++ tds/settings.py
@@ -15,7 +15,7 @@ class Settings(BaseSettings):
     GENERATED_PATH: str = "./tds/autogen"
     DBML_PATH: str = "./askem.dbml"
     SQL_URL: str = "rdb"
-    SQL_PORT: int = 8032
+    SQL_PORT: int = 5432
     SQL_USER: str = "dev"
     SQL_PASSWORD: str = "dev"
     SQL_DB: str = "askem"
