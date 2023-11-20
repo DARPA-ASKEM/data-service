6d3f31e0ddbfada9e644f2472a525fc2261f69de
Powell <powellfendley@gmail.com>
2023-11-20 16:48:33 -0700

    Correction.

--- tds/modules/workflow/model.py
+++ tds/modules/workflow/model.py
@@ -4,20 +4,76 @@ TDS Workflow Model Definition.
 
 from typing import List, Optional
 
-from pydantic import BaseModel
+from pydantic import BaseModel, Enum
 
 from tds.db.base import BaseElasticSearchModel
 from tds.settings import settings
 
 
 class Transform(BaseModel):
-    """
-    Transform data model.
-    """
-
     x: float
     y: float
-    k: float
+    k: Optional[float]
+
+
+class State(BaseModel):
+    modelId: Optional[str]
+    modelConfigurationIds: List[str] = []
+
+
+class OperationType(str, Enum):
+    ModelOperation = "ModelOperation"
+    Dataset = "Dataset"
+    CalibrationOperationCiemss = "CalibrationOperationCiemss"
+    SimulateCiemssOperation = "SimulateCiemssOperation"
+    CalibrateEnsembleCiemms = "CalibrateEnsembleCiemms"
+    SimulateEnsembleCiemss = "SimulateEnsembleCiemss"
+    CalibrationOperationSciml = "CalibrationOperationSciml"
+    SimulateScimlOperation = "SimulateScimlOperation"
+    CalibrateEnsembleSciml = "CalibrateEnsembleSciml"
+    SimulateEnsembleSciml = "SimulateEnsembleSciml"
+
+
+class Output(BaseModel):
+    id: str
+    type: str
+    label: str
+    value: List[str]
+    status: str
+
+
+class Input(BaseModel):
+    id: str
+    type: str
+    label: str
+    status: str
+    value: List[str]
+    acceptMultiple: bool
+
+
+class Node(BaseModel):
+    id: str
+    workflowId: str
+    operationType: OperationType
+    displayName: str
+    x: int
+    y: int
+    state: State
+    inputs: List[Input]
+    outputs: List[Output]
+    statusCode: str
+    width: int
+    height: int
+
+
+class Edge(BaseModel):
+    id: str
+    workflowId: str
+    source: str
+    sourcePortId: str
+    target: str
+    targetPortId: str
+    points: List[Transform]
 
 
 class Workflow(BaseElasticSearchModel):
@@ -25,11 +81,12 @@ class Workflow(BaseElasticSearchModel):
     Workflow Data Model
     """
 
+    id: str
     name: str
     description: Optional[str]
-    nodes: List[dict] = []
-    edges: List[dict] = []
     transform: Transform
+    nodes: List[Node] = []
+    edges: List[Edge] = []
 
     _index = "workflow"
 
